68a3761f33f38b72fd8f2d289f021fd80330be68
coder1408 <ishanyateenjoshi@gmail.com>
2024-02-07 22:10:48 +0530

    Hamming's code (incomplete)

--- Semester 4/CN/Assignment4.py
+++ Semester 4/CN/Assignment4.py
@@ -1,114 +1,86 @@
-import numpy as np
-
-def fill_code(code_word, r, data_word):
-    for i in range(r):
-        code_word[2**i - 1] = 20
-
-    x = 0
-    data_word = np.flip(data_word)
-    for i in range(len(code_word)):
-        if code_word[i] == -1:
-            code_word[i] = data_word[x]
-            x += 1
-    return code_word
-
-def remove_code(code_word, data_word, r):
-    x = 0
-    for i in range(len(code_word)):
-        if i == 2**x - 1:
-            x += 1
-        else:
-            data_word = np.append(data_word, code_word[i])
-
-    data_word = np.flip(data_word)
-    return data_word
-
-def get_pos(r, d):
-    pos = []
-    for i in range(r):
-        temp = []
-        x = 2**i - 1
-        flag = 0
-        while x < r + d:
-            if flag == 0:
-                temp = np.append(temp, np.arange(x, min(x + 2**i, r + d)))
-                x += 2**i
-                flag = 1
-            else:
-                x += 2**i
-                flag = 0
-        pos.append(temp.astype(int))
-
-    return pos
-
-def create_code(data_word):
-    r = 0
-    while 2**r - r < len(data_word) + 1:
+def hamming_encode(data):
+    m = len(data)
+    r = 1
+    while 2**r < m + r + 1:
         r += 1
 
-    code_word = np.full(len(data_word) + r, -1)
-    code_word = fill_code(code_word, r, data_word)
-    pos = get_pos(r, len(data_word))
-    
-    for i in range(r):
-        count = np.count_nonzero(code_word[pos[i]] == 1)
-        if count % 2 == 0:
-            code_word[2**i - 1] = 0
+    # Create a list to store the encoded message with placeholders for parity bits
+    encoded_message = [0] * (m + r)
+    j = 0
+
+    # Fill in the data bits in the encoded message
+    for i in range(1, m + r + 1):
+        if i == 2**j:
+            j += 1
         else:
-            code_word[2**i - 1] = 1
+            # Check if there are still data bits left
+            if j < m:
+                encoded_message[i - 1] = int(data[j])
+                j += 1
 
-    code_word = np.flip(code_word)
-    return code_word
-
-def solve_word(code_word):
-    r = 0
-    while 2**r < len(code_word):
-        r += 1
-    d = len(code_word) - r
-
-    code_word = np.flip(code_word)
-    data_word = np.array([])
-    pos = get_pos(r, d)
-    err = 0
-
+    # Calculate parity bits
     for i in range(r):
-        count = np.count_nonzero(code_word[pos[i]] == 1)
-        if count % 2 != 0:
-            err += 2**i
+        parity_index = 2**i - 1
+        parity = 0
+        for j in range(1, m + r + 1):
+            if (j & (2**i)) == (2**i):
+                parity ^= encoded_message[j - 1]
 
-    if err == 0:
-        print("\nNo error found.\n")
+        encoded_message[parity_index] = parity
+
+    return encoded_message
+
+
+
+def hamming_decode(encoded_message):
+    r = 1
+    while 2**r <= len(encoded_message):
+        r += 1
+
+    # Calculate parity bits
+    for i in range(r):
+        parity_index = 2**i - 1
+        parity = 0
+        for j in range(1, len(encoded_message) + 1):
+            if (j & (2**i)) == (2**i):
+                parity ^= encoded_message[j - 1]
+
+        # If parity doesn't match, flip the bit
+        if parity != encoded_message[parity_index]:
+            encoded_message[parity_index] ^= 1
+
+    decoded_message = [0] * (len(encoded_message) - r)
+    j = 0
+
+    for i in range(1, len(encoded_message) + 1):
+        if i != 2**j:
+            if j < len(decoded_message):
+                decoded_message[j] = encoded_message[i - 1]
+                j += 1
+
+    return decoded_message
+
+
+def simulate_error(encoded_message):
+    error_position = int(input("Enter the position to simulate an error (1-indexed): "))
+    if 1 <= error_position <= len(encoded_message):
+        encoded_message[error_position - 1] ^= 1
+        print("Simulated Error:", encoded_message)
+        return True
     else:
-        print(f"\nError found at pos {d + r - err + 1}.\n")
+        print("Invalid position. No error simulated.")
+        return False
 
-    data_word = remove_code(code_word, data_word, r)
-    return data_word.astype(int)
+# Take user input for the data word
+data = input("Enter the binary data word: ")
+encoded_message = hamming_encode(data)
+print("Code Word:", encoded_message)
 
-def main():
-    n = int(input("Enter data word or code word? (0/1): "))
-    
-    if n == 0:
-        data_word = np.array([], dtype=int)
-        x = int(input("Enter length of data word: "))
-        print("Enter word:")
-        for _ in range(x):
-            a = int(input())
-            data_word = np.append(data_word, a)
-
-        code_word = create_code(data_word)
-        print("\nCode word:")
-        print(" ".join(map(str, code_word)))
-    else:
-        code_word = np.array([], dtype=int)
-        x = int(input("Enter length of code word: "))
-        print("Enter word:")
-        for _ in range(x):
-            a = int(input())
-            code_word = np.append(code_word, a)
-
-        data_word = solve_word(code_word)
-        print("\nData word:")
-        print(" ".join(map(str, data_word)))
-
-if __name__ == "__main__":
-    main()
+# Simulate an error
+if simulate_error(encoded_message):
+    # Decode and correct the error
+    decoded_message = hamming_decode(encoded_message)
+    print("Decoded Message:", decoded_message)
+    print("Error Detected and Corrected.")
+    print("Correct Code Word:", encoded_message)
+    print("Correct Data Word:", ''.join(map(str, decoded_message)))
